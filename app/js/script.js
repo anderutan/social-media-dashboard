030b1411b711f3dbd8c2820bd80ea4c7d6a22bc1
anderu <andrewtan1310@gmail.com>
2024-02-24 16:13:32 +0800

    localStorage color toggle

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,33 @@
 const body = document.querySelector('body');
 const darkToggle = document.getElementById('dark');
+const lightToggle = document.getElementById('light');
 const radioBtns = document.querySelectorAll('.toggle__wrapper input');
+const localGetColor = localStorage.getItem('colorMode');
+
+if (localGetColor) {
+  localGetColor === 'dark' ? setDarkMode() : setLightMode();
+} else {
+  setDarkMode();
+}
 
 for (let btn of radioBtns) {
-  btn.addEventListener('click', (event) => {
-    body.classList = event.target.value;
+  btn.addEventListener('click', () => {
+    if (darkToggle.checked) {
+      localStorage.setItem('colorMode', 'dark');
+      setDarkMode();
+    } else {
+      localStorage.setItem('colorMode', 'light');
+      setLightMode();
+    }
   });
 }
 
-darkToggle.checked ? (body.classList = 'dark') : (body.classList = 'light');
+function setDarkMode() {
+  body.classList = 'dark';
+  darkToggle.click();
+}
+
+function setLightMode() {
+  body.classList = 'light';
+  lightToggle.click();
+}
