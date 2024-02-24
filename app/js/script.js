fead9fa0d3c8d19fb7f1ed11c4ee59b1902febf9
anderu <andrewtan1310@gmail.com>
2024-02-24 15:13:38 +0800

    Add toggle js function

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,11 @@
-console.log('hello');
+const body = document.querySelector('body');
+const darkToggle = document.getElementById('dark');
+const radioBtns = document.querySelectorAll('.toggle__wrapper input');
+
+for (let btn of radioBtns) {
+  btn.addEventListener('click', (event) => {
+    body.classList = event.target.value;
+  });
+}
+
+darkToggle.checked ? (body.classList = 'dark') : (body.classList = 'light');
